4bec1943325c0defb560ddddb9d78d0f20f446c1
Matej Nemček <ybdaba@gmail.com>
2019-08-13 12:46:17 +0200

    build deploy

--- deploy.sh
+++ deploy.sh
@@ -21,8 +21,8 @@ npm run docs:build
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:dysonring/vue-ui-polkadot.git master:gh-pages
-cp -rf docs/.vuepress/dist .
-git  add -A
+cp -rf docs/.vuepress/dist/ .
+git add -A
 git commit -m 'commit build'
-
+git push git@github.com:dysonring/dysonring.github.io.git master
 cd -
